34c8c10381f302ca014ce8ada45fa9ea2425abe8
Karthik S <karthiksgujjar@gmail.com>
2023-07-14 11:57:37 +0530

    Added favicon and some js code for header
    As one scrolls through the webpage the header section elements gets highlighted accordingly with the help of JS code.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const aboutSection = document.getElementById('about');
 const headerTag = document.getElementsByTagName('header')[0];
+const introSection = document.getElementById('intro');
+const aboutSection = document.getElementById('about');
+const projectsSection = document.getElementById('projects');
+const contactSection = document.getElementById('contact');
 const navLinks = document.querySelectorAll('nav>a');
 
-console.log(navLinks)
-
 document.addEventListener('scroll', () => {
+    console.log(introSection.getBoundingClientRect().y,aboutSection.getBoundingClientRect().y,projectsSection.getBoundingClientRect().y,contactSection.getBoundingClientRect().y);
+    if(introSection.getBoundingClientRect().y < 0)
+    {
+        bgColorNeutralizer();
+        headerTag.lastElementChild.children[0].style.backgroundColor='lightblue';
+    }
     if(aboutSection.getBoundingClientRect().y < 0) {
+        bgColorNeutralizer();
+        headerTag.lastElementChild.children[1].style.backgroundColor='lightblue';
         headerTag.style.backgroundColor = 'rgba(0, 0, 0, 1)';
         navLinks.forEach((link) => {
             link.style.color= 'white';
@@ -17,4 +26,21 @@ document.addEventListener('scroll', () => {
             link.style.color= 'black';
         })
     }
+    if(projectsSection.getBoundingClientRect().y < 0){
+        bgColorNeutralizer();
+        headerTag.lastElementChild.children[2].style.backgroundColor='lightblue';
+    }
+    if(contactSection.getBoundingClientRect().y<600){
+        bgColorNeutralizer();
+        headerTag.lastElementChild.children[3].style.backgroundColor='lightblue';
+    }
+
 });
+
+function bgColorNeutralizer(){
+    headerTag.lastElementChild.children[0].style.backgroundColor='rgba(0,0,0,0)';
+    headerTag.lastElementChild.children[1].style.backgroundColor='rgba(0,0,0,0)';
+    headerTag.lastElementChild.children[2].style.backgroundColor='rgba(0,0,0,0)';
+    headerTag.lastElementChild.children[3].style.backgroundColor='rgba(0,0,0,0)';
+
+}
