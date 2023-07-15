2a2aecd06b107f2cc54d5533b82b3d53670daae5
Karthik S <karthiksgujjar@gmail.com>
2023-07-15 21:47:43 +0530

    Updated design
    A theme us set for the content of projects, buttons, links (border-radius)

--- script.js
+++ script.js
@@ -6,27 +6,16 @@ const contactSection = document.getElementById('contact');
 const navLinks = document.querySelectorAll('nav>a');
 
 document.addEventListener('scroll', () => {
-    console.log(introSection.getBoundingClientRect().y,aboutSection.getBoundingClientRect().y,projectsSection.getBoundingClientRect().y,contactSection.getBoundingClientRect().y);
-    if(introSection.getBoundingClientRect().y < 0)
+    if(introSection.getBoundingClientRect().y < 1)
     {
         bgColorNeutralizer();
         headerTag.lastElementChild.children[0].style.backgroundColor='lightblue';
     }
-    if(aboutSection.getBoundingClientRect().y < 0) {
+    if(aboutSection.getBoundingClientRect().y < 1) {
         bgColorNeutralizer();
         headerTag.lastElementChild.children[1].style.backgroundColor='lightblue';
-        headerTag.style.backgroundColor = 'rgba(0, 0, 0, 1)';
-        navLinks.forEach((link) => {
-            link.style.color= 'white';
-        })
     }
-    else{
-        headerTag.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-        navLinks.forEach((link) => {
-            link.style.color= 'black';
-        })
-    }
-    if(projectsSection.getBoundingClientRect().y < 0){
+    if(projectsSection.getBoundingClientRect().y < 1){
         bgColorNeutralizer();
         headerTag.lastElementChild.children[2].style.backgroundColor='lightblue';
     }
@@ -34,7 +23,6 @@ document.addEventListener('scroll', () => {
         bgColorNeutralizer();
         headerTag.lastElementChild.children[3].style.backgroundColor='lightblue';
     }
-
 });
 
 function bgColorNeutralizer(){
@@ -42,5 +30,4 @@ function bgColorNeutralizer(){
     headerTag.lastElementChild.children[1].style.backgroundColor='rgba(0,0,0,0)';
     headerTag.lastElementChild.children[2].style.backgroundColor='rgba(0,0,0,0)';
     headerTag.lastElementChild.children[3].style.backgroundColor='rgba(0,0,0,0)';
-
 }
