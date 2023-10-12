f41677948f7492be65b89edcd9e29dbaf625961b
Karthik S <karthiksgujjar@gmail.com>
2023-10-12 23:46:37 +0530

    Navbar design and Font update
    Navbar is designed to a new and fresh look and websafe font is utilized

--- script.js
+++ script.js
@@ -3,31 +3,72 @@ const introSection = document.getElementById('intro');
 const aboutSection = document.getElementById('about');
 const projectsSection = document.getElementById('projects');
 const contactSection = document.getElementById('contact');
-const navLinks = document.querySelectorAll('nav>a');
+const navLinksSelctOptions = document.getElementById('navLinksSelectOption');
 
 document.addEventListener('scroll', () => {
     if(introSection.getBoundingClientRect().y < 1)
     {
-        bgColorNeutralizer();
-        headerTag.lastElementChild.children[0].style.backgroundColor='#03ABFF';
+        headerTag.style.backgroundColor='black';
+        headerTag.lastElementChild.children[0].style.color='white';
+        headerTag.lastElementChild.children[1].style.color='white';
+        headerTag.lastElementChild.children[2].style.color='white';
+        headerTag.lastElementChild.children[3].style.color='white';
+
+        propertiesNeutralizer();
+        headerTag.lastElementChild.children[0].style.backgroundColor='white';
+        headerTag.lastElementChild.children[0].style.color='black';
+
+        navLinksSelctOptions[0].selected = 'true';
     }
+    else if(introSection.getBoundingClientRect().y > 1){
+    textColorAndHeaderBGColorNeutralizer();
+    
+    }
+
     if(aboutSection.getBoundingClientRect().y < 1) {
-        bgColorNeutralizer();
-        headerTag.lastElementChild.children[1].style.backgroundColor='#03ABFF';
+        propertiesNeutralizer();
+        headerTag.lastElementChild.children[1].style.backgroundColor='white';
+        headerTag.lastElementChild.children[1].style.color='black';
+
+        navLinksSelctOptions[1].selected = 'true';
     }
+
     if(projectsSection.getBoundingClientRect().y < 1){
-        bgColorNeutralizer();
-        headerTag.lastElementChild.children[2].style.backgroundColor='#03ABFF';
+        propertiesNeutralizer();
+        headerTag.lastElementChild.children[2].style.backgroundColor='white';
+        headerTag.lastElementChild.children[2].style.color='black';
+
+        navLinksSelctOptions[2].selected = 'true';
     }
+    
     if(contactSection.getBoundingClientRect().y<600){
-        bgColorNeutralizer();
-        headerTag.lastElementChild.children[3].style.backgroundColor='#03ABFF';
+        propertiesNeutralizer();
+        headerTag.lastElementChild.children[3].style.backgroundColor='white';
+        headerTag.lastElementChild.children[3].style.color='black';
+
+        navLinksSelctOptions[3].selected = 'true';
     }
 });
 
-function bgColorNeutralizer(){
-    headerTag.lastElementChild.children[0].style.backgroundColor='rgba(0,0,0,0)';
-    headerTag.lastElementChild.children[1].style.backgroundColor='rgba(0,0,0,0)';
-    headerTag.lastElementChild.children[2].style.backgroundColor='rgba(0,0,0,0)';
-    headerTag.lastElementChild.children[3].style.backgroundColor='rgba(0,0,0,0)';
+function propertiesNeutralizer(){
+    headerTag.lastElementChild.children[0].style.backgroundColor='transparent';
+    headerTag.lastElementChild.children[0].style.color='white';
+
+    headerTag.lastElementChild.children[1].style.backgroundColor='transparent';
+    headerTag.lastElementChild.children[1].style.color='white';
+
+    headerTag.lastElementChild.children[2].style.backgroundColor='transparent';
+    headerTag.lastElementChild.children[2].style.color='white';
+
+    headerTag.lastElementChild.children[3].style.backgroundColor='transparent';
+    headerTag.lastElementChild.children[3].style.color='white';
+}
+
+function textColorAndHeaderBGColorNeutralizer(){
+    headerTag.style.backgroundColor='transparent';
+
+    headerTag.lastElementChild.children[0].style.color='black';
+    headerTag.lastElementChild.children[1].style.color='black';
+    headerTag.lastElementChild.children[2].style.color='black';
+    headerTag.lastElementChild.children[3].style.color='black';
 }
