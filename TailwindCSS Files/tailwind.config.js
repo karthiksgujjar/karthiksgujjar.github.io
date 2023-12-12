83555b2ec56c9e0dff1782aa0e15e4e73f12840a
Karthik S <karthiksgujjar@gmail.com>
2023-12-12 11:16:47 +0530

    New Design of Portfolio

---
+++ TailwindCSS Files/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "rethinkSans": ['Rethink Sans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
